124cac48771791e4d52139d7d09dc50a96ba381b
Natham Fernandes <natham.fr@gmail.com>
2022-11-21 23:06:35 -0300

    Deu certo.

--- index.js
+++ index.js
@@ -4,24 +4,7 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://api.steampowered.com');
-  
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-  
-    // Pass to next layer of middleware
-    next();
-  });
+app.use(cors());
 
 app.get('/', async (req, res) => {
     res.send("hello! server is working.")
@@ -51,7 +34,7 @@ app.get('/resultados/:matchId/stats', async (req, res) => {
     res.json(stats)
 })
 
-app.get('/jogadores', async (req, res, next) => {
+app.get('/jogadores', async (req, res) => {
     const players = await HLTV.getTopPlayers()
     res.json(players)
 })
